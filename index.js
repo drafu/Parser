64966413405409c6350adaa23fe750b194aa73bd
Egor4ek <egor@4ek.ru>
2018-09-24 10:02:25 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+(() => {
+    console.log('test');
+
+    openFile('file///C:/Users/Egor/Desktop/morozov/1.18l');
+})();
+
+
+var openFile = function (event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function () {
+        var text = reader.result;
+        var node = document.getElementById('output');
+        node.innerText = text;
+        console.log(reader.result.substring(0, 200));
+    };
+    reader.readAsText(input.files[0]);
+};
